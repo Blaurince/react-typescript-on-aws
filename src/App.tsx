49a7badf48dbe4292d0cf05018fb1cbd41646d1c
Barbara Laurince <blaurince@hotmail.com>
2023-05-04 16:26:29 -0400

    running locally

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,27 @@ import React from 'react';
 import { Hello } from './components/Hello';
 import { Form } from './components/Form';
 import './App.css';
+
+
 function App() {
-  return (
+  const [serverData, setServerData] = React.useState([])
+
+  React.useEffect(() => {
+    fetch('http://localhost:4000')
+    .then(res => res.json())
+    .then(data => setServerData(data))
+    .catch(err => console.error(err))
+}, [])
+  return(
     <div className="App">
       <header className="App-header">
      <Hello firstName='Nephie' lastName='Laurince' />
       <Form />
+      {/* @ts-ignore */}
+      {serverData && serverData.map(eachDog => <p>{eachDog.name} -  {eachDog.age}</p>)}
       </header>
     </div>
   )
 }
 
-export default App;
+export default App
